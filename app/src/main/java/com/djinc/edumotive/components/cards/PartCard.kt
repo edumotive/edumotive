87b18cfa144c4045d27e02003c18d5150be64c4c
JayVnl <jorrit.v@outlook.com>
2022-03-28 15:26:49 +0200

    Formed PartCard as seen in our design, also added universal AsyncImage component that uses Coil

--- app/src/main/java/com/djinc/edumotive/components/cards/PartCard.kt
+++ app/src/main/java/com/djinc/edumotive/components/cards/PartCard.kt
@@ -4,16 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.djinc.edumotive.navigation.Screen
+import com.djinc.edumotive.R
+import com.djinc.edumotive.components.AsyncImage
 import com.djinc.edumotive.ui.theme.Background
 import com.djinc.edumotive.ui.theme.PinkPrimary
 import com.djinc.edumotive.ui.theme.PinkSecondary
@@ -31,16 +31,37 @@ fun PartCard(partId: String, partName: String = "", nav: NavController) {
                     }
     ) {
         Column(
-                verticalArrangement = Arrangement.spacedBy(12.dp),
-                modifier = Modifier.padding(12.dp)
+                verticalArrangement = Arrangement.spacedBy(6.dp),
         ) {
             Box(
+                    contentAlignment = Alignment.Center,
                     modifier = Modifier
                             .fillMaxWidth(1f)
                             .aspectRatio(1.0F)
-                            .background(PinkPrimary)
-            )
-            Text(text = partName, style = MaterialTheme.typography.h3)
+                            .padding(start = 12.dp, top = 12.dp, end = 12.dp)
+                            .clip(RoundedCornerShape(8.dp))
+            ) {
+                AsyncImage(imageUrl = "https://picsum.photos/400", imageName = partName)
+            }
+            Text(text = partName, style = MaterialTheme.typography.h3, modifier = Modifier.padding(horizontal = 12.dp))
+            Box(contentAlignment = Alignment.CenterEnd, modifier = Modifier
+                    .fillMaxWidth()
+                    .height(12.dp)
+                    .background(PinkPrimary)) {
+                val negativePadding = 8.dp
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                        .offset(y = -negativePadding)
+                        .requiredHeight(28.dp)
+                        .requiredWidth(28.dp)
+                        .clip(RoundedCornerShape(topStart = 8.dp))
+                        .background(PinkPrimary)
+                        .padding(5.dp)
+                ) {
+                    Icon(painter = painterResource(id = R.drawable.ic_arrows_right), contentDescription = "See details of part: $partName", tint = Background, modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight())
+                }
+            }
         }
     }
 }
