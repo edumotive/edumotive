ee797af59ae86b58e06c2cbf706d1ad7cfe4a4ac
JayVnl <jorrit.v@outlook.com>
2022-03-29 15:15:45 +0200

    Formed exercisesteps as in design

--- app/src/main/java/com/djinc/edumotive/components/ExerciseStep.kt
+++ app/src/main/java/com/djinc/edumotive/components/ExerciseStep.kt
@@ -1,14 +1,21 @@
 package com.djinc.edumotive.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.djinc.edumotive.ui.theme.Background
+import com.djinc.edumotive.ui.theme.PinkPrimary
+import com.djinc.edumotive.ui.theme.fonts
 
 @Composable
 fun ExerciseStep(exerciseStepName: String = "", stepIndex: Int = 0) {
@@ -19,12 +26,22 @@ fun ExerciseStep(exerciseStepName: String = "", stepIndex: Int = 0) {
             modifier = Modifier
                     .fillMaxWidth(1f)
     ) {
-        Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.padding(horizontal = 8.dp, vertical = 12.dp)
-        ) {
-            Text(text = exerciseStepName)
-            Text(text = stepIndex.toString(), style = MaterialTheme.typography.h5)
+        Column() {
+            Text(text = exerciseStepName, style = MaterialTheme.typography.h5, modifier = Modifier.padding(start = 8.dp, top = 12.dp, end = 8.dp, bottom = 8.dp))
+            Box(contentAlignment = Alignment.CenterEnd, modifier = Modifier
+                    .fillMaxWidth()
+                    .height(8.dp)
+                    .background(PinkPrimary)) {
+                val negativePadding = 8.dp
+                Box(contentAlignment = Alignment.Center, modifier = Modifier
+                        .offset(y = -negativePadding)
+                        .requiredHeight(24.dp)
+                        .requiredWidth(24.dp)
+                        .clip(RoundedCornerShape(topStart = 8.dp))
+                        .background(PinkPrimary)) {
+                    Text(text = stepIndex.toString(), fontWeight = FontWeight.Medium, fontFamily = fonts, fontSize = 14.sp, color = Background, modifier = Modifier.padding(top = 2.dp))
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/djinc/edumotive/screens/ExerciseDetails.kt
+++ app/src/main/java/com/djinc/edumotive/screens/ExerciseDetails.kt
@@ -4,13 +4,17 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.djinc.edumotive.R
 import com.djinc.edumotive.components.*
 import com.djinc.edumotive.models.ExerciseStep
 import com.djinc.edumotive.ui.theme.TextSecondary
@@ -20,50 +24,53 @@ import com.djinc.edumotive.utils.WindowSize
 @Composable
 fun ExerciseDetails(exerciseId: String = "", nav: NavController, windowSize: WindowSize) {
     LazyColumn(
-        contentPadding = if (windowSize == WindowSize.Compact) PaddingValues(bottom = 85.dp) else PaddingValues(
-            bottom = 24.dp
-        ),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = Modifier
-            .padding(horizontal = 20.dp)
-            .fillMaxSize()
+            contentPadding = if (windowSize == WindowSize.Compact) PaddingValues(bottom = 85.dp) else PaddingValues(
+                    bottom = 24.dp
+            ),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .fillMaxSize()
     ) {
         item {
             Spacer(modifier = Modifier.height(12.dp))
         }
         item {
             ScreenTitle(title = "Oefening - Naam", spacerHeight = 0)
-            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            Row(horizontalArrangement = Arrangement.spacedBy(16.dp), verticalAlignment = Alignment.CenterVertically) {
                 Text(text = "Hoofdstuk X", color = TextSecondary, fontSize = 16.sp)
-                Text(text = "O 10 - 15 min", color = TextSecondary, fontSize = 16.sp)
+                Row(horizontalArrangement = Arrangement.spacedBy(4.dp), verticalAlignment = Alignment.CenterVertically) {
+                    Icon(painter = painterResource(id = R.drawable.ic_clock), contentDescription = "Clock icon", tint = TextSecondary, modifier = Modifier.padding(bottom = 3.dp))
+                    Text(text = "10 - 15 min", color = TextSecondary, fontSize = 16.sp)
+                }
             }
         }
         item {
             Text(text = "Wat je leert in deze oefening", style = MaterialTheme.typography.h4)
             Text(
-                text = "Amet hendrerit amet, donec vulputate auctor imperdiet curabitur sagittis. Integer vitae id a, nunc, vestibulum consectetur nunc, cursus. Nibh vulputate vitae arcu sed ac eu. Massa ultricies sodales sagittis, consequat, egestas lorem sit.",
-                style = MaterialTheme.typography.body2,
+                    text = "Amet hendrerit amet, donec vulputate auctor imperdiet curabitur sagittis. Integer vitae id a, nunc, vestibulum consectetur nunc, cursus. Nibh vulputate vitae arcu sed ac eu. Massa ultricies sodales sagittis, consequat, egestas lorem sit.",
+                    style = MaterialTheme.typography.body2,
             )
         }
         val exerciseSteps = listOf(
-            ExerciseStep(
-                name = "Stap 1",
-            ),
-            ExerciseStep(
-                name = "Stap 2",
-            ),
-            ExerciseStep(
-                name = "Stap 3",
-            ),
-            ExerciseStep(
-                name = "Stap 4",
-            ),
-            ExerciseStep(
-                name = "Stap 5",
-            ),
+                ExerciseStep(
+                        name = "Stap 1",
+                ),
+                ExerciseStep(
+                        name = "Stap 2",
+                ),
+                ExerciseStep(
+                        name = "Stap 3",
+                ),
+                ExerciseStep(
+                        name = "Stap 4",
+                ),
+                ExerciseStep(
+                        name = "Stap 5",
+                ),
         )
         itemsIndexed(exerciseSteps) { index, item ->
-            ExerciseStep(exerciseStepName = item.name, stepIndex = index)
+            ExerciseStep(exerciseStepName = item.name, stepIndex = index + 1)
         }
     }
 }
