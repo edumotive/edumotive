cac2d97bf43814491ff99a5604aaadeab6765484
JayVnl <jorrit.v@outlook.com>
2022-03-29 11:48:05 +0200

    Created custom bottom navbar

--- app/src/main/java/com/djinc/edumotive/components/cards/PartCard.kt
+++ app/src/main/java/com/djinc/edumotive/components/cards/PartCard.kt
@@ -20,42 +20,42 @@ import com.djinc.edumotive.ui.theme.PinkPrimary
 @Composable
 fun PartCard(partId: String, partName: String = "", imageUrl: String, nav: NavController) {
     Card(
-        backgroundColor = Background,
-        elevation = 3.dp,
-        shape = RoundedCornerShape(8.dp),
-        modifier = Modifier
-            .width(200.dp)
-            .clickable {
-                nav.navigate("part/$partId")
-            }
+            backgroundColor = Background,
+            elevation = 3.dp,
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                    .width(200.dp)
+                    .clickable {
+                        nav.navigate("part/$partId")
+                    }
     ) {
         Column(
-            verticalArrangement = Arrangement.spacedBy(6.dp),
+                verticalArrangement = Arrangement.spacedBy(6.dp),
         ) {
             Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .fillMaxWidth(1f)
-                    .aspectRatio(1.0F)
-                    .padding(start = 12.dp, top = 12.dp, end = 12.dp)
-                    .clip(RoundedCornerShape(8.dp))
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                            .fillMaxWidth(1f)
+                            .aspectRatio(1.0F)
+                            .padding(start = 12.dp, top = 12.dp, end = 12.dp)
+                            .clip(RoundedCornerShape(8.dp))
             ) {
                 AsyncImage(imageUrl = imageUrl, imageName = partName)
             }
             Text(
-                text = partName,
-                style = MaterialTheme.typography.h3,
-                modifier = Modifier.padding(horizontal = 12.dp)
+                    text = partName,
+                    style = MaterialTheme.typography.h3,
+                    modifier = Modifier.padding(horizontal = 12.dp)
             )
             Box(
-                contentAlignment = Alignment.CenterEnd, modifier = Modifier
+                    contentAlignment = Alignment.CenterEnd, modifier = Modifier
                     .fillMaxWidth()
                     .height(12.dp)
                     .background(PinkPrimary)
             ) {
                 val negativePadding = 8.dp
                 Box(
-                    contentAlignment = Alignment.Center, modifier = Modifier
+                        contentAlignment = Alignment.Center, modifier = Modifier
                         .offset(y = -negativePadding)
                         .requiredHeight(28.dp)
                         .requiredWidth(28.dp)
@@ -64,12 +64,12 @@ fun PartCard(partId: String, partName: String = "", imageUrl: String, nav: NavCo
                         .padding(5.dp)
                 ) {
                     Icon(
-                        painter = painterResource(id = R.drawable.ic_arrows_right),
-                        contentDescription = "See details of part: $partName",
-                        tint = Background,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight()
+                            painter = painterResource(id = R.drawable.ic_arrows_right),
+                            contentDescription = "See details of part: $partName",
+                            tint = Background,
+                            modifier = Modifier
+                                    .fillMaxWidth()
+                                    .fillMaxHeight()
                     )
                 }
             }
