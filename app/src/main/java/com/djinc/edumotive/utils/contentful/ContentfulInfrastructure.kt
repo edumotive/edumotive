6ed01f8587904ec2e6b897df2eee9e974c5e81e8
DannyPX <damnoga@hotmail.com>
2022-03-30 13:17:24 +0200

    Get entries from CMS

--- app/src/main/java/com/djinc/edumotive/utils/contentful/ContentfulInfrastructure.kt
+++ app/src/main/java/com/djinc/edumotive/utils/contentful/ContentfulInfrastructure.kt
@@ -1,10 +1,18 @@
 package com.djinc.edumotive.utils.contentful
 
+import com.djinc.edumotive.models.ContentfulModel
+import com.djinc.edumotive.models.ContentfulModelGroup
+
 interface ContentfulInfrastructure {
     val parameter: ContentfulParams
 
-    fun fetchAll(
+    fun fetchAllModels(
         errorCallBack: (Throwable) -> Unit,
-        successCallBack: (String) -> Unit
+        successCallBack: (List<ContentfulModel>) -> Unit
+    )
+
+    fun fetchAllModelGroups(
+        errorCallBack: (Throwable) -> Unit,
+        successCallBack: (List<ContentfulModelGroup>) -> Unit
     )
 }
