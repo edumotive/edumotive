5c7f55d40290fc4394378416c7339f16da4b20f7
DannyPX <damnoga@hotmail.com>
2022-04-11 14:48:20 +0200

    Update sharedPrefs when calling changeLocale

--- app/src/main/java/com/djinc/edumotive/utils/locale.kt
+++ app/src/main/java/com/djinc/edumotive/utils/locale.kt
@@ -2,11 +2,17 @@ package com.djinc.edumotive.utils
 
 import android.content.Context
 import com.djinc.edumotive.MainEdumotive
+import com.djinc.edumotive.R
 import java.util.*
 
 @SuppressWarnings("deprecation")
-fun changeLocale(locale: Locale) {
-    val resources = MainEdumotive.appContext!!.resources
+fun changeLocale(context: Context, locale: Locale) {
+    with (MainEdumotive.sharedPref!!.edit()) {
+        putString(context.getString(R.string.locale), locale.toLanguageTag())
+        apply()
+    }
+
+    val resources = context.resources
     val configuration = resources.configuration
     configuration.setLocale(locale)
     resources.updateConfiguration(configuration, resources.displayMetrics)
