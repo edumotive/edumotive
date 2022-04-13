6e2958add469b2b805bf2f4aeddf8922b25d37a7
JayVnl <jorrit.v@outlook.com>
2022-04-13 10:48:16 +0200

    Showing chapter now instead of exercise name for card and added missing function to refresh/fetch models again after language change

--- app/src/main/java/com/djinc/edumotive/components/cards/ExerciseCard.kt
+++ app/src/main/java/com/djinc/edumotive/components/cards/ExerciseCard.kt
@@ -23,6 +23,7 @@ import com.djinc.edumotive.utils.WindowSize
 fun ExerciseCard(
     exerciseId: String,
     exerciseName: String = "",
+    chapter: String = "",
     imageUrl: String,
     description: String = "",
     fullWidth: Boolean,
@@ -59,7 +60,7 @@ fun ExerciseCard(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = exerciseName,
+                    text = chapter,
                     style = MaterialTheme.typography.caption,
                     modifier = Modifier.padding(start = 12.dp)
                 )
