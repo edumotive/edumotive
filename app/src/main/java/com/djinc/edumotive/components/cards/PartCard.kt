62ae2797508994f89c493fad14706f4ede0bd912
DannyPX <damnoga@hotmail.com>
2022-04-13 11:00:50 +0200

    Refactor unused code and problems

--- app/src/main/java/com/djinc/edumotive/components/cards/PartCard.kt
+++ app/src/main/java/com/djinc/edumotive/components/cards/PartCard.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,7 +18,6 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.djinc.edumotive.R
 import com.djinc.edumotive.components.AsyncImage
-import com.djinc.edumotive.models.ViewModels
 import com.djinc.edumotive.ui.theme.Background
 import com.djinc.edumotive.ui.theme.PinkPrimary
 import com.djinc.edumotive.utils.WindowSize
--- app/src/main/java/com/djinc/edumotive/ui/theme/Theme.kt
+++ app/src/main/java/com/djinc/edumotive/ui/theme/Theme.kt
@@ -16,15 +16,6 @@ private val LightColorPalette = lightColors(
         primary = PinkPrimary,
         primaryVariant = PinkSecondary,
         secondary = BluePrimary
-
-        /* Other default colors to override
-        background = Color.White,
-        surface = Color.White,
-        onPrimary = Color.White,
-        onSecondary = Color.Black,
-        onBackground = Color.Black,
-        onSurface = Color.Black,
-        */
 )
 
 @Composable
