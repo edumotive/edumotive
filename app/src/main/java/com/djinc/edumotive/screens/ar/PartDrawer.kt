79e18596a952d5fa4116e3cf1cffcd988ddcc361
JayVnl <jorrit.v@outlook.com>
2022-04-25 12:46:57 +0200

    Created button for minimizing drawer

--- app/src/main/java/com/djinc/edumotive/screens/ar/PartDrawer.kt
+++ app/src/main/java/com/djinc/edumotive/screens/ar/PartDrawer.kt
@@ -2,17 +2,24 @@ package com.djinc.edumotive.screens.ar
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.djinc.edumotive.R
 import com.djinc.edumotive.components.cards.PartCard
 import com.djinc.edumotive.models.ContentfulModel
 import com.djinc.edumotive.screens.gridItems
@@ -23,6 +30,9 @@ import com.djinc.edumotive.ui.theme.fonts
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PartDrawer(list: List<ContentfulModel>) {
+    val verticalLineWidth = 12.dp
+    val drawerButtonSize = 50.dp
+
     Box(
         modifier = Modifier
             .fillMaxSize(),
@@ -34,7 +44,7 @@ fun PartDrawer(list: List<ContentfulModel>) {
                 .fillMaxWidth(.35f)
                 .background(Background)
         ) {
-            VerticalLine()
+            VerticalLine(lineWidth = verticalLineWidth)
             LazyColumn(
                 contentPadding = PaddingValues(vertical = 24.dp, horizontal = 28.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -61,6 +71,7 @@ fun PartDrawer(list: List<ContentfulModel>) {
                     )
                 }
             }
+            DrawerButton(buttonSize = drawerButtonSize)
         }
     }
 }
@@ -97,14 +108,49 @@ fun <T> LazyListScope.gridItems(
 }
 
 @Composable
-fun VerticalLine() {
-    val width = 12.dp
+fun VerticalLine(lineWidth: Dp) {
     Box(
         modifier = Modifier
-            .offset(x = -width)
+            .offset(x = -lineWidth)
             .fillMaxHeight()
-            .width(width)
+            .width(lineWidth)
             .clip(RoundedCornerShape(topStart = 8.dp, bottomStart = 8.dp))
             .background(PinkPrimary)
     )
+}
+
+@Composable
+fun DrawerButton(buttonSize: Dp) {
+    Box(
+        contentAlignment = Alignment.CenterStart,
+        modifier = Modifier
+            .fillMaxHeight()
+            .offset(x = -buttonSize)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .width(buttonSize)
+                .height(buttonSize)
+                .clip(CircleShape)
+                .background(PinkPrimary)
+                .clickable { }
+        ) {
+            Box(
+                contentAlignment = Alignment.Center, modifier = Modifier
+                    .clip(CircleShape)
+                    .background(Background)
+                    .padding(6.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrows_right),
+                    contentDescription = "Minimize drawer",
+                    modifier = Modifier
+                        .width(20.dp)
+                        .height(20.dp)
+                        .rotate(0f)
+                )
+            }
+        }
+    }
 }
