73550487f5f0bbbff7752bb53cc838e19895a382
DannyPX <damnoga@hotmail.com>
2022-04-25 15:26:38 +0200

    Remove rotation model from text, as it is the parent that it anchors to.

--- app/src/main/java/com/djinc/edumotive/screens/ar/ARFragment.kt
+++ app/src/main/java/com/djinc/edumotive/screens/ar/ARFragment.kt
@@ -120,13 +120,15 @@ class ARFragment : Fragment(R.layout.fragment_ar) {
             }
 
             onFrame = { _ ->
+
+                // Rotate card
                 if(modelSelected.value) {
                     models[selectedModelIndex.value].arModel!!.children.forEach { child ->
                         val cameraPosition = sceneView.camera.worldPosition
                         val cardPosition = child.worldPosition
                         val angle = calcRotationAngleInDegrees(cameraPosition, cardPosition).toFloat()
 
-                        child.rotation = Rotation(0.0f, -angle, 0.0f)
+                        child.rotation = Rotation(0.0f, -angle + models[selectedModelIndex.value].arModel!!.worldRotation.y, 0.0f)
                     }
                 }
             }
@@ -138,44 +140,12 @@ class ARFragment : Fragment(R.layout.fragment_ar) {
         isLoading = true
     }
 
-    /**
-     * Calculates the angle from centerPt to targetPt in degrees.
-     * The return should range from [0,360), rotating CLOCKWISE,
-     * 0 and 360 degrees represents NORTH,
-     * 90 degrees represents EAST, etc...
-     *
-     * Assumes all points are in the same coordinate space.  If they are not,
-     * you will need to call SwingUtilities.convertPointToScreen or equivalent
-     * on all arguments before passing them  to this function.
-     *
-     * @param centerPt   Point we are rotating around.
-     * @param targetPt   Point we want to calcuate the angle to.
-     * @return angle in degrees.  This is the angle from centerPt to targetPt.
-     */
     fun calcRotationAngleInDegrees(centerPt: Position, targetPt: Position): Double {
-        // calculate the angle theta from the deltaY and deltaX values
-        // (atan2 returns radians values from [-PI,PI])
-        // 0 currently points EAST.
-        // NOTE: By preserving Y and X param order to atan2,  we are expecting
-        // a CLOCKWISE angle direction.
         var theta = Math.atan2((targetPt.z - centerPt.z).toDouble(),
             (targetPt.x - centerPt.x).toDouble()
         )
-
-        // rotate the theta angle clockwise by 90 degrees
-        // (this makes 0 point NORTH)
-        // NOTE: adding to an angle rotates it clockwise.
-        // subtracting would rotate it counter-clockwise
         theta += Math.PI / 2.0
-
-        // convert from radians to degrees
-        // this will give you an angle from [0->270],[-180,0]
         var angle = Math.toDegrees(theta)
-
-        // convert to positive range [0-360)
-        // since we want to prevent negative angles, adjust them now.
-        // we can assume that atan2 will not return a negative value
-        // greater than one partial rotation
         if (angle < 0) {
             angle += 360.0
         }
