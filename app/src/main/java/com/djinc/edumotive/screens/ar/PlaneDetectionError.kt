20b225dae541fe658e5c3328d52f35bf783526f4
JayVnl <jorrit.v@outlook.com>
2022-05-12 13:07:54 +0200

    Plane detection error styled for mobile sized devices

--- app/src/main/java/com/djinc/edumotive/screens/ar/PlaneDetectionError.kt
+++ app/src/main/java/com/djinc/edumotive/screens/ar/PlaneDetectionError.kt
@@ -8,21 +8,31 @@ import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.djinc.edumotive.R
+import com.djinc.edumotive.constants.WindowSize
 import com.djinc.edumotive.ui.theme.PinkPrimary
 import com.djinc.edumotive.ui.theme.fonts
 
 @Composable
 fun PlaneDetectionError() {
-    Box(contentAlignment = Alignment.BottomStart, modifier = Modifier.fillMaxSize()) {
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp + 1
+    val windowSize =
+        if (screenWidth < 600) WindowSize.Compact else if (screenWidth < 840) WindowSize.Medium else WindowSize.Expanded
+
+    Box(
+        contentAlignment = if (windowSize == WindowSize.Expanded) Alignment.BottomStart else Alignment.TopEnd,
+        modifier = Modifier.fillMaxSize()
+    ) {
         Row(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(start = 8.dp, bottom = 8.dp)
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 6.dp)
         ) {
             Icon(
                 Icons.Rounded.Warning,
